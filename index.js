66c44ea827cebed13b961682a59e75a81c6401c4
Kim Pham <pham.kim.a@gmail.com>
2018-10-09 08:02:47 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,29 @@
+//produceDrivingRange() - Returns a function that then calculates whether a given trip is within range. 
+function produceDrivingRange(range) {
+    return function(block1, block2) {
+        num1 = parseInt(block1.slice(0, block1.length - 2))
+        num2 = parseInt(block2.slice(0, block2.length - 2))
+        const blocks = Math.abs(num1 - num2)
+        return blocks < range ? `within range by ${range - blocks}` : `${blocks - range} blocks out of range` ;
+    }
+}
+
+//produceTipCalculator() - Returns a function that then calculates a tip.
+function produceTipCalculator(percentage) {
+    return function(fare) {
+        return fare * percentage;
+    }
+}
+
+//createDriver is a function that returns a Driver class. The class has reference to a driverId 
+//that is incremented each time a new driver is created.
+function createDriver() {
+    driverId = 0;
+
+    return class {
+        constructor(name) {
+            this.name = name;
+            this.id = ++driverId;
+        }
+    }
+}
